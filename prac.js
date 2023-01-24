6cf2fca89c27e479db40b991bd9edf86f0b832ef
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2023-01-24 16:20:20 +0300

    context: execution

---
+++ prac.js
@@ -0,0 +1,11 @@
+class residents{
+    constructor(name, age, location){
+        this.name = name
+        this.age = age
+        this.location = location
+        console.log(this)
+    }
+}
+
+new residents("diana", 43, "nanyu")
+new residents("nuki", 3, "Manu")
